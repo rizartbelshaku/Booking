b577fedd7edaf889e5067e9d5ebd619ff0c70691
rizartb <114589444+rizartbelshaku@users.noreply.github.com>
2024-01-30 23:34:57 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace RezervimHoteli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoEllipsis = true;
+            this.label1.BackColor = System.Drawing.Color.SlateGray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(325, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(577, 593);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(338, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " 👤  EMRI :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(338, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 31);
+            this.label4.TabIndex = 3;
+            this.label4.Text = " 📅 DATA E REZERVIMIT";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.CalendarForeColor = System.Drawing.Color.LightGreen;
+            this.dateTimePicker1.Location = new System.Drawing.Point(593, 199);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(279, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(338, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(234, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "  📅 DATA E LESHIMIT";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.CalendarForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dateTimePicker2.Location = new System.Drawing.Point(593, 294);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(262, 22);
+            this.dateTimePicker2.TabIndex = 6;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(342, 384);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(191, 23);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "   🛏 ZGJIDH DHOMEN";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(341, 423);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(188, 72);
+            this.checkedListBox1.TabIndex = 8;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(338, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 23);
+            this.label7.TabIndex = 9;
+            this.label7.Text = " 📩  : MAIL";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(338, 246);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(102, 23);
+            this.label9.TabIndex = 11;
+            this.label9.Text = " 🕣 TIME:";
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label11.Location = new System.Drawing.Point(342, 331);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(102, 23);
+            this.label11.TabIndex = 13;
+            this.label11.Text = " 🕣 TIME:";
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label13.Location = new System.Drawing.Point(664, 384);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(191, 23);
+            this.label13.TabIndex = 15;
+            this.label13.Text = "   👥 Persona";
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label14.Location = new System.Drawing.Point(496, 48);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(165, 34);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "MIRESEVINI";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightCyan;
+            this.textBox1.Location = new System.Drawing.Point(455, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightCyan;
+            this.textBox2.Location = new System.Drawing.Point(455, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 22);
+            this.textBox2.TabIndex = 19;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightCyan;
+            this.textBox3.Location = new System.Drawing.Point(455, 247);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 22);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Azure;
+            this.textBox4.Location = new System.Drawing.Point(455, 331);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 22);
+            this.textBox4.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(542, 561);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 43);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "REZEVO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.BackColor = System.Drawing.Color.LightCyan;
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(667, 423);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(188, 72);
+            this.checkedListBox2.TabIndex = 24;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1197, 635);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.Text = "HOTEL SHARRI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RezervimHoteli
+{
+    public partial class Form1 : Form
+    {
+        private string emriRezervimit;
+        private DateTime dataRezervimit;
+        private DateTime dataeLeshitmit;
+        private List<string> dhomatHotelit = new List<string>();
+        private List<string> persona = new List<string>();
+        private List<Rezervimi> listaRezervimeve = new List<Rezervimi>();
+        private List<DhomatRezervuara> dhomatRezervuara = new List<DhomatRezervuara>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            InicializoDhomat();
+            InicializoPersona();
+           
+
+        }
+       
+        
+
+        private struct DhomatRezervuara
+        {
+            public DateTime DataRezervimit;
+            public DateTime DataLargimit;
+            public List<string> Dhomat;
+
+        }
+
+        private void InicializoDhomat()
+        {
+            checkedListBox1.Items.Add("Dhoma 101 / 50$");
+            checkedListBox1.Items.Add("Dhoma 102 / 70$");
+            checkedListBox1.Items.Add("Dhoma 103/80$");
+            checkedListBox1.Items.Add("Dhoma 104/ 90$");
+            checkedListBox1.Items.Add("Dhoma 105 /120$");
+            checkedListBox1.Items.Add("Dhoma 106 / 1404");
+        }
+        private void InicializoPersona()
+        {
+            checkedListBox2.Items.Add(" 1 Person");
+            checkedListBox2.Items.Add(" 2 Person");
+            checkedListBox2.Items.Add(" 3 Person");
+            checkedListBox2.Items.Add(" 4 Person");
+            checkedListBox2.Items.Add(" 5 Person");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+            label1.BackColor = Color.Transparent;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            emriRezervimit = textBox1.Text;
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dataRezervimit = dateTimePicker1.Value;
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            dataeLeshitmit = dateTimePicker2.Value;
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            dhomatHotelit.Clear();
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                dhomatHotelit.Add(item.ToString());
+            }
+        }
+
+        private void checkedListBox2_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            persona.Clear();
+            foreach (var item in checkedListBox2.CheckedItems)
+            {
+                persona.Add(item.ToString());
+            }
+        }
+      
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string teksti = textBox2.Text;
+        }
+
+
+        public class Rezervimi
+        {
+            public string Emri { get; set; }
+            public DateTime DataRezervimit { get; set; }
+            public DateTime DataLargimit { get; set; }
+            public List<string> Dhomat { get; set; }
+            public List<string> Persona { get; set; }
+
+            public Rezervimi(string emri, DateTime dataRezervimit, DateTime dataLargimit, List<string> dhomat, List<string> persona)
+            {
+                Emri = emri;
+                DataRezervimit = dataRezervimit;
+                DataLargimit = dataLargimit;
+                Dhomat = dhomat;
+                Persona = persona;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count == 0 || checkedListBox2.CheckedItems.Count == 0 || string.IsNullOrEmpty(emriRezervimit))
+            {
+                MessageBox.Show("Ju lutemi zgjidhni një dhomë, një numër personash dhe plotësoni emrin para se të kryeni rezervimin.");
+                return;
+            }
+
+            else
+            {
+                dhomatHotelit.Clear();
+                foreach (var item in checkedListBox1.CheckedItems)
+                {
+                    dhomatHotelit.Add(item.ToString());
+                }
+
+                persona.Clear();
+                foreach (var item in checkedListBox2.CheckedItems)
+                {
+                    persona.Add(item.ToString());
+                }
+            }
+         
+            if (DhomatErezervuara(dhomatHotelit))
+            {
+                MessageBox.Show("Dhoma e zgjedhur është e rezervuar nga personi i parë. Ju lutemi zgjidhni një dhomë të lirë.");
+                return;
+            }
+            else
+            {
+               
+                Rezervimi rezervimi = new Rezervimi(emriRezervimit, dataRezervimit, dataeLeshitmit, dhomatHotelit, persona);
+                listaRezervimeve.Add(rezervimi);
+                DhomatRezervuara dhomatRezervuaraPersonit = new DhomatRezervuara
+                {
+                    DataRezervimit = dataRezervimit,
+                    DataLargimit = dataeLeshitmit,
+                    Dhomat = new List<string>(dhomatHotelit)
+                };
+                dhomatRezervuara.Add(dhomatRezervuaraPersonit);
+            }
+
+            MessageBox.Show($"Rezervimi për {emriRezervimit} u krye me sukses!\n\nDhoma e zgjedhur: {string.Join(", ", dhomatHotelit)}\nNumri i personave: {string.Join(", ", persona)}\nData e rezervimit: {dataRezervimit}\nData e largimit: {dataeLeshitmit}");
+        }
+
+        private bool DhomatErezervuara(List<string> dhomatHotelit)
+        {
+            foreach (var dhomatRezervuaraPersonit in dhomatRezervuara)
+            {
+                if (dhomatRezervuaraPersonit.Dhomat.Intersect(dhomatHotelit).Any())
+                {
+                    return true; 
+                }
+            }
+            return false;
+        }
+
+      
+    }
+}
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
